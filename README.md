bd58189c8b39bd503dac1378a0d4bf958b19e810
Benny Powers <web@bennypowers.com>
2018-03-14 21:49:40 +0200

    Initial commit 🎂

---
+++ README.md
@@ -0,0 +1,20 @@
+# \<lazy-image\>
+
+Lazily load your images
+
+## Usage
+
+
+Lazy Image loads an image lazily!
+
+## Installation
+```
+npm -i -S @power-elements/lazy-image
+```
+
+## Usage
+```html
+<script async type="module" src="/node_modules/@power-elements/lazy-image/lazy-image.js"></script>
+<lazy-image src="/image.jpg" alt="Lazy Image"></lazy-image>
+```
+
---
+++ lazy-image.js
@@ -0,0 +1,100 @@
+import { LitElement, html } from '/node_modules/@polymer/lit-element/lit-element.js';
+import { compose, map, prop, some } from './lib.js';
+
+/**
+ * Lazy Image loads an image lazily!
+ *
+ * ## Usage
+ * ```html
+ * <lazy-image src="/image.jpg" alt="Lazy Image"></lazy-image>
+ * ```
+ */
+class LazyImage extends LitElement {
+
+  static get properties() {
+    return {
+			/** Alt value for image. */
+      alt: String,
+
+			/** Whether or not the image has intersected. */
+      intersecting: Boolean,
+
+			/** Placeholder image string. */
+      placeholder: String,
+
+			/** Margin into the image which determines when the IntersectionObserver fires. */
+      rootMargin: String,
+
+			/** img src */
+      src: String,
+
+			/** img src */
+      fade: Boolean,
+
+			/** Threshold at which to trigger intersection. */
+      threshold: Number,
+    };
+  }
+
+  constructor() {
+    super();
+    this.intersecting = false;
+    this.threshold = this.threshold || 0;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    const {rootMargin, threshold} = this;
+
+    const setIntersecting = b => (this.intersecting = b || this.intersecting, b);
+
+		const unloadIfIntersecting = intersecting => {
+			if (intersecting) this.observer = null;
+		};
+
+    this.observer = new IntersectionObserver(
+			compose(
+				unloadIfIntersecting,
+				setIntersecting,
+				some(x => x),
+				map(prop('isIntersecting'))
+			),
+			{rootMargin, threshold}
+		);
+
+		this.observer
+			.observe(this);
+  }
+
+  render({alt, intersecting, placeholder, src, fade}) {
+    return html`
+			<style>
+			:host {
+				display: block;
+				width: 100%;
+			}
+
+			img {
+				display: block;
+				width: 100%;
+			}
+
+			.fade {
+				opacity: 0;
+				transition: opacity 0.5s ease;
+			}
+
+			.intersecting {
+				opacity: 1;
+			}
+		  </style>
+
+      <img alt$="${alt}"
+          src$="${intersecting ? src : placeholder}"
+          class$="${intersecting ? 'intersecting' : ''} ${fade ? 'fade' : ''}"/>`;
+  }
+}
+
+customElements.define('lazy-image', LazyImage);
+
