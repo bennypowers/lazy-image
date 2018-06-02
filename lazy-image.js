4641052b61e8962306797752fd226c0a1a936525
Benny Powers <web@bennypowers.com>
2018-06-03 00:41:24 +0300

    Refactors code

--- lazy-image.js
+++ lazy-image.js
@@ -1,7 +1,11 @@
-import { LitElement, html } from '../../@polymer/lit-element/lit-element.js';
-import { compose, identity } from '../power-functions/combinators.js';
-import { map, some } from '../power-functions/array.js';
-import { prop } from '../power-functions/object.js';
+import {LitElement, html} from '../../@polymer/lit-element/lit-element.js';
+import {compose, identity} from '../power-functions/combinators.js';
+import {map, some} from '../power-functions/array.js';
+import {prop} from '../power-functions/object.js';
+
+const constant = (x) => () => x;
+const when = (p, f) => (x) => p(x) ? f(x) : constant(x);
+const isTrue = (p) => !!p;
 
 /**
  * Lazy Image loads an image lazily!
@@ -12,27 +16,20 @@ import { prop } from '../power-functions/object.js';
  * ```
  */
 class LazyImage extends LitElement {
-
   static get properties() {
     return {
       /** Alt value for image. */
       alt: String,
-
       /** Whether or not the image has intersected. */
       intersecting: Boolean,
-
       /** Placeholder image string. */
       placeholder: String,
-
-      /** Margin into the image which determines when the IntersectionObserver fires. */
+      /** Margin around the element within which the IntersectionObserver fires. */
       rootMargin: String,
-
       /** img src */
       src: String,
-
-      /** img src */
+      /** Whether or not the image should fade into view. */
       fade: Boolean,
-
       /** Threshold at which to trigger intersection. */
       threshold: Number,
     };
@@ -46,32 +43,25 @@ class LazyImage extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+    const {rootMargin, threshold} = this;
+    const unload = () => this.observer = null;
+    const setIntersecting = (b) =>
+      (this.intersecting = (b || this.intersecting), b);
 
-    const {
-      rootMargin,
-      threshold
-    } = this;
-
-    const setIntersecting = b => (this.intersecting = b || this.intersecting, b);
-
-    const unloadIfIntersecting = intersecting => {
-      if (intersecting) this.observer = null;
-    };
-
-    this.observer = new IntersectionObserver(
-      compose(
-        unloadIfIntersecting,
-        setIntersecting,
-        some(identity),
-        map(prop('isIntersecting'))
-      ), { rootMargin, threshold }
+    // Sets `intersecting` property on the element, then unloads the observer.
+    const callback = compose(
+      when(isTrue, unload),
+      setIntersecting,
+      some(identity),
+      map(prop('isIntersecting'))
     );
 
-    this.observer
-      .observe(this);
+    this.observer = new IntersectionObserver(callback, {rootMargin, threshold});
+    this.observer.observe(this);
   }
 
   _render({alt, fade, intersecting, placeholder, src}) {
+    const url = intersecting ? src : placeholder;
     return html `
       <style>
       :host {
@@ -86,19 +76,17 @@ class LazyImage extends LitElement {
         object-fit: var(--lazy-image-img-object-fit, contain);
       }
 
-      .fade {
+      [fade] {
         opacity: 0;
         transition: opacity 0.5s ease;
       }
 
-      .intersecting {
+      [intersecting] {
         opacity: 1;
       }
     </style>
 
-    <img alt$="${alt}"
-        src$="${intersecting ? src : placeholder}"
-        class$="${intersecting ? 'intersecting' : ''} ${fade ? 'fade' : ''}"/>`;
+    <img alt$="${alt}" intersecting?="${intersecting}" fade?="${fade}" src$="${url}"/>`;
   }
 }
 
